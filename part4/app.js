85971bda0cc2f733003d085392bdc6ed04717946
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2025-08-14 14:54:33 +0530

    completed exercise: 4.2

---
+++ part4/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogRouter = require('./controllers/blog')
+
+const app = express()
+
+logger.info('connection to', config.MONGODB_URI)
+
+mongoose
+    .connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
+
+app.use(express.json())
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
