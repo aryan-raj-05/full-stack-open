a587bc3f4925ee363774f38a91eb0587adf88714
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2025-08-14 11:08:37 +0530

    separated configs to utils directory

--- part4/index.js
+++ part4/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+const config = require('./utils/config')
 const express = require('express')
 const mongoose = require('mongoose')
 
@@ -13,7 +13,7 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(express.json())
@@ -32,7 +32,7 @@ app.post('/api/blogs', (request, response) => {
   })
 })
 
-const PORT = process.env.PORT
+const PORT = config.PORT
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
