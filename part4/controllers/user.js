140fc0b44c0d4c475a48ac1768a85992f0676b81
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2025-08-23 20:21:06 +0530

    added data validation of username and passwords

--- part4/controllers/user.js
+++ part4/controllers/user.js
@@ -4,6 +4,9 @@ const usersRouter = require('express').Router()
 
 usersRouter.post('/', async (request, response) => {
     const { username, password, name } = request.body
+    if (!password || password.length < 3) {
+        return response.status(400).json({ error: 'password length should be atleast 3'})
+    }
 
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(password, saltRounds)
