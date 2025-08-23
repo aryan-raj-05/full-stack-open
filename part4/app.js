0318cb2c1e4c4247475a5b1b1fe3a55b2726d5f4
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2025-08-23 12:27:28 +0530

    completed exercise: 4.15
    - implemented: create users with post request
    - implemented: get list of users with get request

--- part4/app.js
+++ part4/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogRouter = require('./controllers/blog')
+const usersRouter = require('./controllers/user')
 const middleware = require('./utils/middleware')
 
 const app = express()
@@ -21,6 +22,7 @@ mongoose
 app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.errorHandler)
 
