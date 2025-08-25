ea8f1cf9fab36017a903142094071db20301b22f
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2025-08-25 18:54:06 +0530

    added login request handler using JWT

--- part4/app.js
+++ part4/app.js
@@ -4,6 +4,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogRouter = require('./controllers/blog')
 const usersRouter = require('./controllers/user')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 const app = express()
@@ -23,6 +24,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
 
