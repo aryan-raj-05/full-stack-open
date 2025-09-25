d2127cab0742800ec5fcd97e338b76c1778abf75
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2025-09-25 21:57:52 +0530

    completed exercise: 5.14

--- part5/src/components/Blog.test.jsx
+++ part5/src/components/Blog.test.jsx
@@ -1,30 +1,43 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders title and author but not url or likes by default', () => {
-  const blog = {
-    title: 'Testing React components',
-    author: 'Kent C. Dodds',
-    url: 'https://testing-library.com/',
-    likes: 10,
-    user: { name: 'testuser' }
-  }
+describe('<Blog />', () => {
+  beforeEach(() => {
+    const blog = {
+      title: 'Testing React components',
+      author: 'Kent C. Dodds',
+      url: 'https://testing-library.com/',
+      likes: 10,
+      user: { name: 'testuser' }
+    }
 
-  const loggedInUser = 'testuser'
-  const mockLike = vi.fn()
-  const mockDelete = vi.fn()
+    const loggedInUser = 'testUser'
+    const mockLike = vi.fn()
+    const mockDelete = vi.fn()
 
-  render(
-    <Blog
-      blog={blog}
-      loggedInUser={loggedInUser}
-      onLike={mockLike}
-      onDelete={mockDelete}
-    />
-  )
+    render(
+      <Blog
+        blog={blog}
+        loggedInUser={loggedInUser}
+        onLike={mockLike}
+        onDelete={mockDelete}
+      />
+    )
+  })
 
-  expect(screen.getByText('Testing React components Kent C. Dodds')).toBeDefined()
+  test('renders title and author but not url or likes by default', () => {
+    expect(screen.getByText('Testing React components Kent C. Dodds')).toBeDefined()
+    expect(screen.getByText('https://testing-library.com/')).not.toBeVisible()
+    expect(screen.getByText('10')).not.toBeVisible()
+  })
 
-  expect(screen.queryByText('https://testing-library.com/')).not.toBeVisible()
-  expect(screen.queryByText('10')).not.toBeVisible()
+  test('when view button is clicked url and likes are visible', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    expect(screen.getByText('https://testing-library.com/')).toBeDefined()
+    expect(screen.getByText('10')).toBeVisible()
+  })
 })
